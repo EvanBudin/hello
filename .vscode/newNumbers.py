8d0f453d432d1c6f6047142d8c1fee102a9966fe
EvanBudin <evanfbudin@gmail.com>
2020-12-04 14:47:05 -0600

    added a counterReset function to newNumbers

--- .vscode/newNumbers.py
+++ .vscode/newNumbers.py
@@ -1,19 +1,21 @@
 # https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/ for the challenge
+def counterReset():
+    counter = 0
 inputNumber = input("input a number ")
 numberList = (list(inputNumber))
 endNumber = ""
-counter = 0
+counterReset()
 while counter < len(numberList):
     numberList[counter] = int(numberList[counter])
     counter = counter + 1
 print (numberList)
-counter = 0
+counterReset()
 while counter < len(numberList):
     numberList[counter] = (numberList[counter] + 1)
     numberList[counter] = str(numberList[counter])
     counter = counter + 1
     print (numberList)
-counter = 0
+counterReset()
 while counter < len(numberList):
     endNumber = endNumber + numberList[counter]
     counter = counter + 1
