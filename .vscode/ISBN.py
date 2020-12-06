322e3d1f832d8b2287814fae0fd79eba6e976095
EvanBudin <evanfbudin@gmail.com>
2020-12-06 13:56:54 -0600

    added a function for the multiplication

--- .vscode/ISBN.py
+++ .vscode/ISBN.py
@@ -1,3 +1,14 @@
+def isbnMultiply():
+    originalList[0] = originalList[0] * 10
+    originalList[1] = originalList[1] * 9
+    originalList[2] = originalList[2] * 8
+    originalList[3] = originalList[3] * 7
+    originalList[4] = originalList[4] * 6
+    originalList[5] = originalList[5] * 5
+    originalList[6] = originalList[6] * 4
+    originalList[7] = originalList[7] * 3
+    originalList[8] = originalList[8] * 2
+    
 originalNumber = input("Enter ISBN ")
 originalList = list(originalNumber)
 intList = []
@@ -8,15 +19,7 @@ if len(originalList) == 10:
     while counter < len(originalList):
         originalList[counter] = int(originalList[counter])
         counter = counter + 1
-    originalList[0] = originalList[0] * 10
-    originalList[1] = originalList[1] * 9
-    originalList[2] = originalList[2] * 8
-    originalList[3] = originalList[3] * 7
-    originalList[4] = originalList[4] * 6
-    originalList[5] = originalList[5] * 5
-    originalList[6] = originalList[6] * 4
-    originalList[7] = originalList[7] * 3
-    originalList[8] = originalList[8] * 2
+    isbnMultiply()
     answer = sum(originalList)
     if answer % 11 == 0:
         print(True)
