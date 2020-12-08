461657e576bf5b37f67de7f686d6bb092dce8d7c
EvanBudin <evanfbudin@gmail.com>
2020-12-07 18:46:15 -0600

    changed things and changed them back

--- .vscode/kinterRoller.py
+++ .vscode/kinterRoller.py
@@ -1,5 +1,7 @@
 import random
 from tkinter import *
+def reset():
+    myList= []
 def getBoth():
     counter = 0
     diceAnswer = diceEntry.get()
@@ -14,12 +16,15 @@ def getBoth():
     printAnswer.grid(row=4,column=1)
     endGame= Label(root, text= myList)
     endGame.grid(row=3,column=1)
+
 myList = []
 root = Tk()
 root.geometry("500x200")
 diceEntry= Entry(root, width=20)
 typeEntry= Entry(root, width=20)
-button= Button(root, width=10,height=5, text="ROLL!", command= getBoth)
+button= Button(root, width=10,height=5, text="ROLL", command= getBoth)
+resetButton= Button(root, width=10,height=5, text="Reset", command= reset)
+resetButton.grid(row= 5, column=1)
 typeQuestion= Label(root, text="How many sides do these dice have?")
 diceQuestion= Label(root, text="How many dice would you like to roll?")
 button.grid(row=2,column=1)
