ed474d6e936e0e1e88208c6870e5d1e15464f9fa
EvanBudin <evanfbudin@gmail.com>
2020-12-08 15:40:43 -0600

    added functions for aging

--- .vscode/betterRabbits.py
+++ .vscode/betterRabbits.py
@@ -1,3 +1,15 @@
+def ageFemale():
+    counter = 95
+    for z in reversed(femaleRabbits):
+        femaleRabbits[counter] = femaleRabbits[counter - 1]
+        counter = counter - 1
+
+def ageMale():
+    counter = 95
+    for z in reversed(maleRabbits):
+        maleRabbits[counter] = maleRabbits[counter - 1]
+        counter = counter -1
+
 init = input("Enter Rabbit Values: ")
 init = init.split(" ")
 init = [int(x) for x in init]
@@ -16,22 +28,17 @@ totalRabbitsNeeded = init[2]
 totalRabbits = sum(femaleRabbits) + sum(maleRabbits)
 
 while totalRabbits < totalRabbitsNeeded:
-    counter = 95
     fertileRabbits = sum(femaleRabbits)- femaleRabbits[0]-femaleRabbits[1]-femaleRabbits[2]-femaleRabbits[3]
     maleBabyHolder = fertileRabbits*5
     femaleBabyHolder = fertileRabbits*9
-    for z in reversed(femaleRabbits):
-        femaleRabbits[counter] = femaleRabbits[counter - 1]
-        counter = counter - 1
+    ageFemale()
     dead = dead + femaleRabbits[0]
     femaleRabbits[0] = femaleBabyHolder
-    counter = 95
-    for z in reversed(maleRabbits):
-        maleRabbits[counter] = maleRabbits[counter - 1]
-        counter = counter -1
+    ageMale()
     dead = dead + maleRabbits[0]
     maleRabbits[0] = maleBabyHolder
     runCounter = runCounter+1
     totalRabbits = sum(maleRabbits) + sum(femaleRabbits)
 print(runCounter)
 print(dead)
+
