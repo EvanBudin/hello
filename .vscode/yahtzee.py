f804c23428ff9b506eeb71f90f53eae1f36c56c4
EvanBudin <evanfbudin@gmail.com>
2020-12-09 13:38:00 -0600

    working yahtzee

--- .vscode/yahtzee.py
+++ .vscode/yahtzee.py
@@ -1,7 +1,7 @@
 intake = input("Enter a series of dice rolls: ")
 intake = intake.split(", ")
-intake = [int(i) for i in intake]
-intake.append(0)
-intake[0],intake[1],intake[2],intake[3],intake[4],intake[5] = 6*intake.count(6),5*intake.count(5),4*intake.count(4),3*intake.count(3),2*intake.count(2),1*intake.count(1)
+print(intake)
+intake = [int(x) for x in intake]
+intake = [x * intake.count(x) for x in intake]
 intake.sort(reverse = True)
 print(intake[0])
