6733796826456486e560f2562c0b21f0b95ab91d
EvanBudin <evanfbudin@gmail.com>
2020-12-10 17:45:47 -0600

    shortened scale

--- .vscode/scale.py
+++ .vscode/scale.py
@@ -2,28 +2,26 @@ majorMinor = input("Major or Minor?: ")
 
 if majorMinor == "Major":
     noteIntake = input("Enter What Major We Are In?: ")
+    numList = {}
+    numList["Do"] = 0
+    numList["Re"] = 2
+    numList["Mi"] = 4
+    numList["Fa"] = 5
+    numList["So"] = 7
+    numList["La"] = 9
+    numList["Ti"] = 11
 elif majorMinor == "Minor":
     noteIntake = input("Enter What Minor We Are In?: ")
+    numList = {}
+    numList["Do"] = 0
+    numList["Re"] = 2
+    numList["Mi"] = 3
+    numList["Fa"] = 5
+    numList["So"] = 7
+    numList["La"] = 8
+    numList["Ti"] = 10
 
 doIntake = input("What Pitch Are You Looking For?: ")
-
-majorNumList = {}
-majorNumList["Do"] = 0
-majorNumList["Re"] = 2
-majorNumList["Mi"] = 4
-majorNumList["Fa"] = 5
-majorNumList["So"] = 7
-majorNumList["La"] = 9
-majorNumList["Ti"] = 11
-
-minorNumList = {}
-minorNumList["Do"] = 0
-minorNumList["Re"] = 2
-minorNumList["Mi"] = 3
-minorNumList["Fa"] = 5
-minorNumList["So"] = 7
-minorNumList["La"] = 8
-minorNumList["Ti"] = 10
 notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
 
 while noteIntake != notes[0]:
@@ -32,10 +30,6 @@ while noteIntake != notes[0]:
     for i in reversed(notes):
         notes[counter] = notes[counter - 1]
         counter = counter -1
-        print(notes)
     notes[0] = holder
 print(notes)
-if majorMinor == "Major":
-    print(notes[majorNumList[doIntake]])
-elif majorMinor == "Minor":
-    print(notes[minorNumList[doIntake]])
+print(notes[numList[doIntake]])
