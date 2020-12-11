4d7ee94258bef1edd83c99ff446ed85db7dd95c1
EvanBudin <evanfbudin@gmail.com>
2020-12-11 13:36:44 -0600

    Fixed the roller and changed name

--- .vscode/rollerGUI.py
+++ .vscode/rollerGUI.py
@@ -1,13 +1,12 @@
 import random
 from tkinter import *
-def reset():
-    myList= []
 def getBoth():
     counter = 0
     diceAnswer = diceEntry.get()
     typeAnswer = typeEntry.get()
     diceAnswer = int(diceAnswer)
     typeAnswer = int(typeAnswer)
+    myList = []
     while counter < diceAnswer:
         myList.append(random.randint(1,typeAnswer))
         counter = counter + 1
@@ -22,9 +21,7 @@ root = Tk()
 root.geometry("500x200")
 diceEntry= Entry(root, width=20)
 typeEntry= Entry(root, width=20)
-button= Button(root, width=10,height=5, text="ROLL", command= getBoth)
-resetButton= Button(root, width=10,height=5, text="Reset", command= reset)
-resetButton.grid(row= 5, column=1)
+button= Button(root, width=15,height=5, text="ROLL", command= getBoth)
 typeQuestion= Label(root, text="How many sides do these dice have?")
 diceQuestion= Label(root, text="How many dice would you like to roll?")
 button.grid(row=2,column=1)
