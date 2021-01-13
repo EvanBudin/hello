7599dfa0276d2480d6fe9633a1e37e9c347bb603
EvanBudin <evanfbudin@gmail.com>
2021-01-12 20:00:52 -0600

    fixed cribbage code

--- .vscode/crib.py
+++ .vscode/crib.py
@@ -39,7 +39,6 @@ numList = [int(i) for i in numList]
 numList.sort()
 print(numList)
 
-
 def fifteens(numList,points):
     counter = 0
     for i in numList:
@@ -143,117 +142,248 @@ def fifteens(numList,points):
 # This function calculates points scored by runs by checking for every possible run in the game
 def run(numList, points):
     numList = [str(i) for i in numList]
-    numList = numList[0] + numList[1] + numList[2] + numList[3] + numList[4]
-    if numList.find("123") != -1:
-        if numList.find("1234") != -1:
-            if numList.find("12345") != -1:
+
+    if (numList[0] == "1" or numList[1] == "1" or numList[2] == "1" or numList[3] == "1" or numList[4] == "1") and (numList[0] == "2" or numList[1] == "2" or numList[2] == "2" or numList[3] == "2" or numList[4] == "2") and (numList[0] == "3" or numList[1] == "3" or numList[2] == "3" or numList[3] == "3" or numList[4] == "3"):
+        if numList[0] == "4" or numList[1] == "4" or numList[2] == "4" or numList[3] == "4" or numList[4] == "4":
+            if numList[0] == "5" or numList[1] == "5" or numList[2] == "5" or numList[3] == "5" or numList[4] == "5":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("1") == 2 or numList.count("2") == 2 or numList.count("3") == 2 or numList.count("4") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("234") != -1:
-        if numList.find("2345") != -1:
-            if numList.find("23456") != -1:
+            if numList.count("1") == 3 or numList.count("2") == 3 or numList.count("3") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("1") == 2 and (numList.count("2") == 2 or numList.count("3") == 2)) or (numList.count("2") == 2 and numList.count("3") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "2" or numList[1] == "2" or numList[2] == "2" or numList[3] == "2" or numList[4] == "2") and (numList[0] == "3" or numList[1] == "3" or numList[2] == "3" or numList[3] == "3" or numList[4] == "3") and (numList[0] == "4" or numList[1] == "4" or numList[2] == "4" or numList[3] == "4" or numList[4] == "4"):
+        if numList[0] == "5" or numList[1] == "5" or numList[2] == "5" or numList[3] == "5" or numList[4] == "5":
+            if numList[0] == "6" or numList[1] == "6" or numList[2] == "6" or numList[3] == "6" or numList[4] == "6":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("2") == 2 or numList.count("3") == 2 or numList.count("4") == 2 or numList.count("5") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("345") != -1:
-        if numList.find("3456") != -1:
-            if numList.find("34567") != -1:
+            if numList.count("2") == 3 or numList.count("3") == 3 or numList.count("4") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("2") == 2 and (numList.count("3") == 2 or numList.count("4") == 2)) or (numList.count("3") == 2 and numList.count("4") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "3" or numList[1] == "3" or numList[2] == "3" or numList[3] == "3" or numList[4] == "3") and (numList[0] == "4" or numList[1] == "4" or numList[2] == "4" or numList[3] == "4" or numList[4] == "4") and (numList[0] == "5" or numList[1] == "5" or numList[2] == "5" or numList[3] == "5" or numList[4] == "5"):
+        if numList[0] == "6" or numList[1] == "6" or numList[2] == "6" or numList[3] == "6" or numList[4] == "6":
+            if numList[0] == "7" or numList[1] == "7" or numList[2] == "7" or numList[3] == "7" or numList[4] == "7":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("3") == 2 or numList.count("4") == 2 or numList.count("5") == 2 or numList.count("6") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("456") != -1:
-        if numList.find("4567") != -1:
-            if numList.find("45678") != -1:
+            if numList.count("3") == 3 or numList.count("4") == 3 or numList.count("5") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("3") == 2 and (numList.count("4") == 2 or numList.count("5") == 2)) or (numList.count("4") == 2 and numList.count("5") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "4" or numList[1] == "4" or numList[2] == "4" or numList[3] == "4" or numList[4] == "4") and (numList[0] == "5" or numList[1] == "5" or numList[2] == "5" or numList[3] == "5" or numList[4] == "5") and (numList[0] == "6" or numList[1] == "6" or numList[2] == "6" or numList[3] == "6" or numList[4] == "6"):
+        if numList[0] == "7" or numList[1] == "7" or numList[2] == "7" or numList[3] == "7" or numList[4] == "7":
+            if numList[0] == "8" or numList[1] == "8" or numList[2] == "8" or numList[3] == "8" or numList[4] == "8":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("4") == 2 or numList.count("5") == 2 or numList.count("6") == 2 or numList.count("7") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("567") != -1:
-        if numList.find("5678") != -1:
-            if numList.find("56789") != -1:
+            if numList.count("4") == 3 or numList.count("5") == 3 or numList.count("6") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("4") == 2 and (numList.count("5") == 2 or numList.count("6") == 2)) or (numList.count("5") == 2 and numList.count("6") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "5" or numList[1] == "5" or numList[2] == "5" or numList[3] == "5" or numList[4] == "5") and (numList[0] == "6" or numList[1] == "6" or numList[2] == "6" or numList[3] == "6" or numList[4] == "6") and (numList[0] == "7" or numList[1] == "7" or numList[2] == "7" or numList[3] == "7" or numList[4] == "7"):
+        if numList[0] == "8" or numList[1] == "8" or numList[2] == "8" or numList[3] == "8" or numList[4] == "8":
+            if numList[0] == "9" or numList[1] == "9" or numList[2] == "9" or numList[3] == "9" or numList[4] == "9":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("5") == 2 or numList.count("6") == 2 or numList.count("7") == 2 or numList.count("8") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("678") != -1:
-        if numList.find("6789") != -1:
-            if numList.find("678910") != -1:
+            if numList.count("5") == 3 or numList.count("6") == 3 or numList.count("7") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("5") == 2 and (numList.count("6") == 2 or numList.count("7") == 2)) or (numList.count("6") == 2 and numList.count("7") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+
+
+    elif (numList[0] == "6" or numList[1] == "6" or numList[2] == "6" or numList[3] == "6" or numList[4] == "6") and (numList[0] == "7" or numList[1] == "7" or numList[2] == "7" or numList[3] == "7" or numList[4] == "7") and (numList[0] == "8" or numList[1] == "8" or numList[2] == "8" or numList[3] == "8" or numList[4] == "8"):
+        if numList[0] == "9" or numList[1] == "9" or numList[2] == "9" or numList[3] == "9" or numList[4] == "9":
+            if numList[0] == "10" or numList[1] == "10" or numList[2] == "10" or numList[3] == "10" or numList[4] == "10":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("6") == 2 or numList.count("7") == 2 or numList.count("8") == 2 or numList.count("9") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("789") != -1:
-        if numList.find("78910") != -1:
-            if numList.find("7891011") != -1:
+            if numList.count("6") == 3 or numList.count("7") == 3 or numList.count("8") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("6") == 2 and (numList.count("7") == 2 or numList.count("8") == 2)) or (numList.count("7") == 2 and numList.count("8") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "7" or numList[1] == "7" or numList[2] == "7" or numList[3] == "7" or numList[4] == "7") and (numList[0] == "8" or numList[1] == "8" or numList[2] == "8" or numList[3] == "8" or numList[4] == "8") and (numList[0] == "9" or numList[1] == "9" or numList[2] == "9" or numList[3] == "9" or numList[4] == "9"):
+        if numList[0] == "10" or numList[1] == "10" or numList[2] == "10" or numList[3] == "10" or numList[4] == "10":
+            if numList[0] == "11" or numList[1] == "11" or numList[2] == "11" or numList[3] == "11" or numList[4] == "11":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("7") == 2 or numList.count("8") == 2 or numList.count("9") == 2 or numList.count("10") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("8910") != -1:
-        if numList.find("891011") != -1:
-            if numList.find("89101112") != -1:
+            if numList.count("7") == 3 or numList.count("8") == 3 or numList.count("9") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("7") == 2 and (numList.count("8") == 2 or numList.count("9") == 2)) or (numList.count("8") == 2 and numList.count("9") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "8" or numList[1] == "8" or numList[2] == "8" or numList[3] == "8" or numList[4] == "8") and (numList[0] == "9" or numList[1] == "9" or numList[2] == "9" or numList[3] == "9" or numList[4] == "9") and (numList[0] == "10" or numList[1] == "10" or numList[2] == "10" or numList[3] == "10" or numList[4] == "10"):
+        if numList[0] == "11" or numList[1] == "11" or numList[2] == "11" or numList[3] == "11" or numList[4] == "11":
+            if numList[0] == "12" or numList[1] == "12" or numList[2] == "12" or numList[3] == "12" or numList[4] == "12":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("8") == 2 or numList.count("9") == 2 or numList.count("10") == 2 or numList.count("11") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("91011") != -1:
-        if numList.find("9101112") != -1:
-            if numList.find("910111213") != -1:
+            if numList.count("8") == 3 or numList.count("9") == 3 or numList.count("10") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("8") == 2 and (numList.count("9") == 2 or numList.count("10") == 2)) or (numList.count("9") == 2 and numList.count("10") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "9" or numList[1] == "9" or numList[2] == "9" or numList[3] == "9" or numList[4] == "9") and (numList[0] == "10" or numList[1] == "10" or numList[2] == "10" or numList[3] == "10" or numList[4] == "10") and (numList[0] == "11" or numList[1] == "11" or numList[2] == "11" or numList[3] == "11" or numList[4] == "11"):
+        if numList[0] == "12" or numList[1] == "12" or numList[2] == "12" or numList[3] == "12" or numList[4] == "12":
+            if numList[0] == "13" or numList[1] == "13" or numList[2] == "13" or numList[3] == "13" or numList[4] == "13":
                 points = points + 5
                 print("run of 5")
             else:
                 points = points + 4
-                print("run of 4")
+                if numList.count("9") == 2 or numList.count("10") == 2 or numList.count("11") == 2 or numList.count("12") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("101112") != -1:
-        if numList.find("10111213") != -1:
-            points = points + 4
-            print("run of 4")
+            if numList.count("9") == 3 or numList.count("10") == 3 or numList.count("11") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("9") == 2 and (numList.count("10") == 2 or numList.count("11") == 2)) or (numList.count("10") == 2 and numList.count("11") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "10" or numList[1] == "10" or numList[2] == "10" or numList[3] == "10" or numList[4] == "10") and (numList[0] == "11" or numList[1] == "11" or numList[2] == "11" or numList[3] == "11" or numList[4] == "11") and (numList[0] == "12" or numList[1] == "12" or numList[2] == "12" or numList[3] == "12" or numList[4] == "12"):
+        if numList[0] == "13" or numList[1] == "13" or numList[2] == "13" or numList[3] == "13" or numList[4] == "13":
+                points = points + 4
+                if numList.count("10") == 2 or numList.count("11") == 2 or numList.count("12") == 2 or numList.count("13") == 2:
+                    points = points + 4
+                    print("double run of 4")
+                else:
+                    print("run of 4")
         else:
             points = points + 3
-            print("run of 3")
-    elif numList.find("111213") != -1:
-        points = points + 3
-        print("run of 3")
+            if numList.count("10") == 3 or numList.count("11") == 3 or numList.count("12") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("10") == 2 and (numList.count("11") == 2 or numList.count("12") == 2)) or (numList.count("11") == 2 and numList.count("12") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
+    elif (numList[0] == "11" or numList[1] == "11" or numList[2] == "11" or numList[3] == "11" or numList[4] == "11") and (numList[0] == "12" or numList[1] == "12" or numList[2] == "12" or numList[3] == "12" or numList[4] == "12") and (numList[0] == "13" or numList[1] == "13" or numList[2] == "13" or numList[3] == "13" or numList[4] == "13"):
+            points = points + 3
+            if numList.count("11") == 3 or numList.count("12") == 3 or numList.count("13") == 3:
+                points = points + 6
+                print("triple run of 3")
+            elif (numList.count("11") == 2 and (numList.count("12") == 2 or numList.count("13") == 2)) or (numList.count("12") == 2 and numList.count("13") == 2):
+                points = points + 9
+                print("quadruple run of 3")
+            else:
+                print("run of 3")
+
     return points
+
 # This function checks for pairs by seeing if there is more than one of each card, and adds the amount of points divided by the number of cards needed per card
 def pairs(hand, points):
     if hand.count(hand[0]) == 2:
